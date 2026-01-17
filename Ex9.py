05610abae5e0670bf6d3c0f1a41e7f75562199ee
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2026-01-18 00:01:48 +0530

    Add files via upload

---
+++ Ex9.py
@@ -0,0 +1,17 @@
+#Method 3: Palindrome in Python using Recursion
+
+def is_palindrome(n, temp, rev=0):
+    if n == 0:
+        if temp == rev:
+            return "The number is a palindrome!"
+        else:
+            return "The number isn't a palindrome!"
+    else:
+        dig = n % 10
+        rev = rev * 10 + dig
+        n = n // 10
+        return is_palindrome(n, temp, rev)
+ 
+n = int(input("Enter number:"))
+result = is_palindrome(n, n)
+print(result)
